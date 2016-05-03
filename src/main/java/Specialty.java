36ddfc006dbf33b980b1f3d08a18b719f5d50586
Anthony Gold and DJ Roychoudhury <dj@>
2016-05-03 11:12:02 -0700

    create initial project files, pass intantiation tests for Specialty.

---
+++ src/main/java/Specialty.java
@@ -0,0 +1,16 @@
+public class Specialty {
+  // private int id;
+  private String type;
+
+  public Specialty(String type) {
+    this.type = type;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  // public int getId() {
+  //   return id;
+  // }
+}
---
+++ src/test/SpecialtyTest.java
@@ -0,0 +1,31 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SpecialtyTest {
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:portgresql://localhost:5432/doctors_office_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM specialties *;";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
+
+  @Test
+  public void Specialty_instantiatesCorrectly_true() {
+    Specialty mySpecialty = new Specialty("General Care");
+    assertEquals(true, mySpecialty instanceof Specialty);
+  }
+
+  @Test
+  public void getType_specialtyInstantiatesWithType_String() {
+    Specialty mySpecialty = new Specialty("General Care");
+    assertEquals("General Care", mySpecialty.getType());
+  }
+}
